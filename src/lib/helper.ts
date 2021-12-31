23ce715746541a8157030b934fb428a11f9e4b5e
Allan MacGregor <dev+github@allanmacgregor.com>
2021-12-31 18:56:40 -0500

    Update main helper - add checkNsInstance

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -10,6 +10,7 @@ import { NS } from "../../NetscriptDefinitions"
 export const waitFor = async (ns: NS, callback: () => any, timeout = 60000): Promise<any> => {
   let timer = 0
   while (timer < timeout) {
+    // eslint-disable-next-line callback-return
     const result = await callback()
     if (result !== undefined) {
       return result
@@ -42,10 +43,36 @@ export function announce(ns: NS, message: string, toastStyle: string): void {
   if (toastStyle) ns.toast(message, toastStyle)
 }
 
+/** @param {NS} ns
+ * Returns a helpful error message if we forgot to pass the ns instance to a function */
+ export function checkNsInstance(ns: NS, fnName = "this function"): NS {
+    // eslint-disable-next-line no-throw-literal
+    if (!ns.print) throw `The first argument to ${fnName} should be a 'ns' instance.`
+    return ns
+  }
+
+/** Helper to log a message, and optionally also tprint it and toast it
+ * @param {NS} ns - The nestcript instance passed to your script's main entry point */
+export function log(ns: NS, message = "", alsoPrintToTerminal = false, toastStyle = "", maxToastLength = 100): string {
+  checkNsInstance(ns, '"log"')
+  ns.print(message)
+  if (alsoPrintToTerminal) ns.tprint(message)
+  if (toastStyle) ns.toast(message.length <= maxToastLength ? message : message.substring(0, maxToastLength - 3) + "...", toastStyle)
+  return message
+}
+
 /**
- * Simplify disabling multiple types of logs on a script 
- * @param {NS} ns 
+ * Simplify disabling multiple types of logs on a script
+ * @param {NS} ns
  **/
 export function disableLogs(ns: NS, listOfLogs: Array<string>): void {
   ["disableLog"].concat(...listOfLogs).forEach((log) => ns.disableLog(log))
+}
+
+/** Generate a hashCode for a string that is pretty unique most of the time */
+export function hashCode(s: string): number {
+  return s.split("").reduce(function (a, b) {
+    a = (a << 5) - a + b.charCodeAt(0)
+    return a & a
+  }, 0)
 }
