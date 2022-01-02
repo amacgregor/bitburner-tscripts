6271e0eb4f53786aa20ca3b5295e9a3163422f9b
Allan MacGregor <dev+github@allanmacgregor.com>
2022-01-01 19:02:01 -0500

    Add flurry script with manualHack ability

---
+++ src/flurry/payloads/manualhack.ts
@@ -0,0 +1,10 @@
+import { NS } from '../../../NetscriptDefinitions'
+
+/** @param {NS} ns **/
+export async function main(ns: NS): Promise<void> {
+  const server = ns.args[0]
+  const sleeptime = ns.args[1]
+  await ns.sleep(sleeptime)
+  ns.connect(server)
+  await ns.manualHack(server)
+}
