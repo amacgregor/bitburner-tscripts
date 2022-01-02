50655ec6e8b8f1339136537cfe28253662603a06
Allan MacGregor <dev+github@allanmacgregor.com>
2022-01-02 12:44:16 -0500

    Add table maker

---
+++ src/lib/common/tableMaker.ts
@@ -0,0 +1,19 @@
+export const tableMaker = (table: string[][]) => {
+  const lengths: number[] = [];
+  for (const row of table) {
+    for (const [index, col] of row.entries()) {
+      if (index === row.length - 1) {
+        continue;
+      }
+      lengths[index] = Math.max(lengths[index] ?? 0, col.length);
+    }
+  }
+
+  return table.map((row) => {
+    return row
+      .map((col, index) => {
+        return col.padStart(lengths[index], " ");
+      })
+      .join("  ");
+  });
+};
