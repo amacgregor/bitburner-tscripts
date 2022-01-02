f9e3e978e7a39d57c1a77c2b1dd34d03621b1f84
Allan MacGregor <dev+github@allanmacgregor.com>
2022-01-01 22:22:55 -0500

    Fix payloads for flurry
    - Update for typescript errors

--- src/flurry/payloads/grow.ts
+++ src/flurry/payloads/grow.ts
@@ -2,8 +2,8 @@ import { NS } from '../../../NetscriptDefinitions'
 
 /** @param {NS} ns **/
 export async function main(ns: NS): Promise<void> {
-  const server = ns.args[0]
-  const sleeptime = ns.args[1]
+  const server: any = ns.args[0]
+  const sleeptime: any = ns.args[1]
   await ns.sleep(sleeptime)
   await ns.grow(server)
 }
--- src/flurry/payloads/manualhack.ts
+++ src/flurry/payloads/manualhack.ts
@@ -2,9 +2,9 @@ import { NS } from '../../../NetscriptDefinitions'
 
 /** @param {NS} ns **/
 export async function main(ns: NS): Promise<void> {
-  const server = ns.args[0]
-  const sleeptime = ns.args[1]
+  const server: any = ns.args[0]
+  const sleeptime: any = ns.args[1]
   await ns.sleep(sleeptime)
   ns.connect(server)
-  await ns.manualHack(server)
+  await ns.manualHack()
 }
